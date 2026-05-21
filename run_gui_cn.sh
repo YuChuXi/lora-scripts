60f225f53358ebe064b907b06795891b89f43871
YuChuXi <cyu245656@gmail.com>
2026-05-21 16:41:26 +0800

    Update .gitignore, set executable permissions on scripts, and remove empty .keep file

--- run_gui_cn.sh
+++ run_gui_cn.sh
@@ -2,10 +2,10 @@
 
 source "./venv/bin/activate"
 
-export HF_HOME=huggingface
-export HF_ENDPOINT=https://hf-mirror.com
-export PIP_INDEX_URL="https://pypi.tuna.tsinghua.edu.cn/simple"
-export PYTHONUTF8=1
+export HF_HOME
+export HF_ENDPOINT
+export PIP_INDEX_URL
+export PYTHONUTF8
 
 python gui.py "$@"
 
